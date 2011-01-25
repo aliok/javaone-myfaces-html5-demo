b77ea5397e124b305520b7acaff86d05086c2295
unknown <adm@.(none)>
2011-01-25 02:41:20 +0200

    Changed the syntax highligter tool. Now using Prettyifier.

--- src/main/webapp/resources/script/demo/common.js
+++ src/main/webapp/resources/script/demo/common.js
@@ -13,3 +13,19 @@ function submitForm(formId){
        return false;
    }
 }
+
+function removeCDataAndTrim(){
+    var elementsByClassName = document.getElementsByClassName('prettyprint');
+    var ua = navigator.userAgent;
+    var isOpera = parseFloat(ua.split('Opera/')[1]) || undefined;
+    for (var i=0; i<elementsByClassName.length; i++) {
+        var innerHTML = elementsByClassName[i].innerHTML;
+        if(isOpera)     //behaves different in Opera
+            innerHTML = innerHTML.substr(10, innerHTML.length - 16);
+        else
+            innerHTML = innerHTML.substr(11, innerHTML.length - 16);
+        innerHTML = innerHTML.replace(new RegExp("^[\\s]+", "g"), "");
+        innerHTML = innerHTML.replace(new RegExp("[\\s]+$", "g"), "");
+        elementsByClassName[i].innerHTML = innerHTML;
+    }
+}
